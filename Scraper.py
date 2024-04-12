f2c1a8c955163da7c014e45363df89f90a7bb0da
TStew2003 <97974576+TStew2003@users.noreply.github.com>
2024-04-12 18:56:51 -0400

    Fixed bugs to only include popular games on Stema

---
+++ Scraper.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://store.steampowered.com/"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+    popular_game_containers = soup.find("div", id= "tab_topsellers_content")
+
+    if popular_game_containers:
+         game_containers = popular_game_containers.find_all("a", class_="tab_item")
+    
+    with open("popular_games.txt", "w", encoding="utf-8") as file:
+        for game in game_containers:
+            game_title = game.find("div", class_="tab_item_name")
+            if game_title:
+                file.write(game_title.text.strip() + "\n")
+            else: 
+                print("Game title not found.")
+
+else: 
+    print("Failed to retreive from Steam.")
